ac55e5a688ef9f8cde2268282d8bf7718489cee1
jammer <jammer@jammer.fi>
2022-06-28 04:43:08 +0300

    Exercise 1.02

---
+++ project/src/main.rs
@@ -0,0 +1,22 @@
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use std::env;
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let port : u16 = env::var("PORT").unwrap_or_else(|_| "8080".to_string())
+        .parse::<u16>().expect("Invalid PORT environment variable.");
+    println!("Server started in port {}", port);
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+    })
+    .bind(("127.0.0.1", port))?
+        .run()
+        .await
+}
+
