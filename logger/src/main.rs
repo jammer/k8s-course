e1ce4f231a0000ea2ac2ae47e2e34e30b2247e85
jammer <jammer@jammer.fi>
2022-06-30 18:19:03 +0300

    Exercise 1.07

--- logger/src/main.rs
+++ logger/src/main.rs
@@ -1,13 +1,29 @@
 use chrono::prelude::*;
 use uuid::Uuid;
-use std::thread::sleep;
-use std::time::Duration;
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+use std::env;
 
-fn main() {
-    let id = Uuid::new_v4();
-    loop {
-        let time = Utc::now();
-        println!("{:?} {}", time, id);
-        sleep(Duration::from_secs(5));
-    }
+#[get("/")]
+async fn hello(start: web::Data<Uuid>) -> impl Responder {
+    let time = Utc::now();
+    let resp = format!("{:?} {}",time,start.get_ref());
+    println!("{}",resp);
+    HttpResponse::Ok().body(resp)
 }
+
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let port : u16 = env::var("PORT").unwrap_or_else(|_| "3000".to_string())
+        .parse::<u16>().expect("Invalid PORT environment variable.");
+    println!("Server started in port {}", port);
+    HttpServer::new(|| {
+        App::new()
+            .app_data(web::Data::new(Uuid::new_v4()))
+            .service(hello)
+    })
+    .bind(("0.0.0.0", port))?
+        .run()
+        .await
+}
+
