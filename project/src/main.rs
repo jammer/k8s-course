eed056ff13e56e7987deb5769f7ba55eb6a1db84
jammer <jammer@jammer.fi>
2022-07-01 15:04:57 +0300

    Exercise 1.12

--- project/src/main.rs
+++ project/src/main.rs
@@ -1,6 +1,8 @@
 use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
 use std::env;
 use rand::{distributions::Alphanumeric, Rng};
+use std::fs;
+use actix_web::http::header::ContentType;
 
 fn random() -> String {
     rand::thread_rng()
@@ -12,11 +14,28 @@ fn random() -> String {
 
 #[get("/")]
 async fn hello(start: web::Data<String>) -> impl Responder {
+    let dt = chrono::Utc::today();
+    let fmt = format!("/data/{}.jpg",dt);
+    let file = std::path::Path::new(&fmt);
+    if !file.exists() {
+        let bytes = reqwest::get("https://picsum.photos/300").await.unwrap().bytes().await.unwrap();
+        fs::write(file,bytes);
+    }
     println!("Request!");
-    let resp = format!("Hello {} from {}",random(),start.get_ref());
-    HttpResponse::Ok().body(resp)
+    HttpResponse::Ok().insert_header(ContentType::html()).body("<img src=\"picture.jpg\">")
 }
 
+#[get("/picture.jpg")]
+async fn picture(start: web::Data<String>) -> impl Responder {
+    let dt = chrono::Utc::today();
+    let fmt = format!("/data/{}.jpg",dt);
+    let file = std::path::Path::new(&fmt);
+    if !file.exists() {
+        println!("Picture not here");
+        return HttpResponse::NotFound().body("");
+    }
+    HttpResponse::Ok().body(fs::read(file).unwrap())
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -27,6 +46,7 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .app_data(web::Data::new(random()))
             .service(hello)
+            .service(picture)
     })
     .bind(("0.0.0.0", port))?
         .run()
